82129a4ceac98bd364cea0b20002ee010b88b1e2
John Behan <behanj@gmail.com>
2016-03-18 15:49:07 +0700

    fixed for deploying to heroku, dotenv for port

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var path = process.cwd();
 var express = require('express');
 var routes = require(path + '/app/routes/index.js');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use('/public', express.static(path + '/public'));
 app.use('/controllers', express.static(path + '/app/controllers'));
 
 routes(app);
 
-app.listen(8080, function () {
-  console.log('Node.js listening on port 8080...');
+app.listen(port,  function () {
+  console.log('Node.js listening on port ' + port + '...');
 });
